c95c705bc4cf5bc6e702cbb2c43ae454cd8e0c2e
ElBurroViejo <simon.david.altmann@gmail.com>
2018-02-08 21:30:06 +0100

    worked on api call

--- geo_fetch.py
+++ geo_fetch.py
@@ -1,5 +1,6 @@
 import requests
-
+import time
+import pandas as pd
 import get_credentials 
 
 cred = get_credentials.credentials
@@ -9,8 +10,8 @@ google_geo_key = cred.google_places_key
 
 params = {
 
-                   "location" : [-33.8670522,151.1957362]
-                  , "radius"  : 300
+                   "location" : [52.4848525,13.356632]
+                  , "radius"  : 600
                   , "type" : "restaurant"
                   , "key" : google_geo_key
                     }
@@ -24,15 +25,42 @@ def create_client(params, output = "json"):
     string_list = [ "location=" +",".join(map(str,params["location"])) ]
 
     for key in params:
-        if key != "location" and "key" != key:
+        if not key in  ["location", "key", "pagetoken"]:
             string_list.append( key + "=" + str(params[key]))
 
 
     string_list.append("key=" + params["key"])
+    if "pagetoken" in params:
+        string_list= [ "key=" + params["key"] , "pagetoken=" + params["pagetoken"]]
+
 
     base_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/"
     client = base_url + output + "?" + "&".join(string_list) 
     return client
 
-print (create_client(params))
-a = (requests.get(create_client(params)))
+
+def execute_request():
+    
+    
+    global_results = []
+    
+    while True:
+        print(len(global_results))
+        time.sleep(5)
+        response = requests.get(create_client(params))
+        json_file = response.json()
+        
+        global_results.extend(json_file["results"])
+        if "next_page_token" in json_file:
+            params["pagetoken"] = json_file["next_page_token"]
+        else:
+            break
+    print(len(global_results)) 
+    return global_results
+test =  execute_request()
+
+#print (create_client(params))
+#a = (requests.get(create_client(params)))
+#
+#json_file = a.json()["results"]
+#df = pd.DataFrame(json_file)
