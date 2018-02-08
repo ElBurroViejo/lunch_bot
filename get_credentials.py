55f8400a4e8b003edc2bbff6adb3bcdbec522a70
ElBurroViejo <simon.david.altmann@gmail.com>
2018-02-08 22:55:31 +0100

    added more description for credentials

--- get_credentials.py
+++ get_credentials.py
@@ -1,9 +1,16 @@
+"""
+This can be used as small helper file so that you do not have to save credentials
+in clear text in Github (kind of a no no ;))
+Just create a file credentials.py in a folder where the sys.path points.
+Then add credentials like this:
+
+google_places_key = SOME_KEY
+
+"""
 
 import sys
 
-
 sys.path.append("../")
-
 
 import credentials 
 
